ebadc682d08a9dfd9fef8999aec70d1f6ef981e1
Tilman Holube <tilman@holube.de>
2025-12-03 16:24:46 +0100

    fix permissions in docker

--- scripts/build-css.sh
+++ scripts/build-css.sh
@@ -8,6 +8,7 @@ docker build -t scss:local ./docker/scss
 echo "Building CSS from SCSS..."
 docker run --rm --name scss \
   -w /app \
+  -u "${UID}":"${GID}" \
   -v "$(pwd)":/app \
   scss:local sh -c "sass src/scss:site/css --style compressed --no-source-map"
 
