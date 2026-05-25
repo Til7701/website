78f106125ea5142d385169f355224fd4cb979b29
Tilman Holube <tilman@holube.de>
2026-05-25 21:43:17 +0200

    Update swagger ui to v5.32.6

--- openapi/bundle.sh
+++ openapi/bundle.sh
@@ -13,13 +13,13 @@ docker run --rm \
   -i /local/src/openapi.yaml \
   -o /local/target/bundle
 
-  docker run --rm \
-    -u "${UID}":"${GID}" \
-    -v "${PWD}":/local \
-     openapitools/openapi-generator-cli:v7.17.0 generate \
-    -g openapi \
-    -i /local/src/openapi.yaml \
-    -o /local/target/bundle
+docker run --rm \
+  -u "${UID}":"${GID}" \
+  -v "${PWD}":/local \
+   openapitools/openapi-generator-cli:v7.17.0 generate \
+  -g openapi \
+  -i /local/src/openapi.yaml \
+  -o /local/target/bundle
 
 cp target/bundle/openapi/openapi.yaml ../site/api/v1/openapi.yaml
 cp target/bundle/openapi.json ../site/api/v1/openapi.json
